cbf7fee31cfb2c5fb0495b13520c1b68550455ba
Ashish Srivastava <Ashi.Srivastava@gmail.com>
2022-01-11 20:43:24 +0530

    Initial commit

---
+++ S3bucketDemo/Models/EnumModel.cs
@@ -0,0 +1,11 @@
+﻿namespace S3bucketDemo.Models
+{
+    public enum UploadFileName
+    {
+        First = 1,
+        Second = 2,
+        Third = 3,
+        Fourth = 4,
+        Fifth = 5,
+    }
+}
---
+++ S3bucketDemo/Models/ServiceConfiguration.cs
@@ -0,0 +1,11 @@
+﻿namespace S3bucketDemo.Models
+{
+    public class ServiceConfiguration
+    {
+        public AWSS3Configuration AWSS3 { get; set; }
+    }
+    public class AWSS3Configuration
+    {
+        public string BucketName { get; set; }
+    }
+}
